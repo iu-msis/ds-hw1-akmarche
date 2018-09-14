a6fb1fe87ef84e872fad19817ea4c913bd6955eb
akmarche <42591895+akmarche@users.noreply.github.com>
2018-09-14 19:03:55 -0400

    Reformating and Vue.js structuring
    Cleaned code and added Vue structure to project file.

---
+++ public/public/js/app.js
@@ -0,0 +1,12 @@
+var foodieSiteApp = new Vue({
+  el: '#foodie',
+  data: {
+    site: {
+      "name" :  'DELIGHTFUL DISHES',
+      "description" : 'Foodie blog for the culinary adventurer',
+    }
+    guestRecommendationTable [
+
+    ]
+  }
+})
