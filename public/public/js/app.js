c1045eea2d1b6bff5828c4f7babb234bfdfb636c
akmarche <42591895+akmarche@users.noreply.github.com>
2018-09-14 21:15:23 -0400

    Randomuser.me
    Added randomuser.me ajax script and create link to script from guest recommendation page.

--- public/public/js/app.js
+++ public/public/js/app.js
@@ -6,7 +6,11 @@ var foodieSiteApp = new Vue({
       "description" : 'Foodie blog for the culinary adventurer',
     }
     guestRecommendationTable [
-
+      {
+        "id": 0,
+        "guest_name": '',
+        "guest_age": '',
+      }
     ]
   }
 })
