5899c21792459d66a65efd59ba2dba5b39886fbb
akmarche <42591895+akmarche@users.noreply.github.com>
2018-10-10 07:49:11 -0400

    Comment.php code
    Added code to models --> Comment.php to declare and instantiate variables "id" and "comment"

---
+++ app/models/Comment.php
@@ -0,0 +1,40 @@
+<?php
+class Comment
+{
+  public $id;
+  public $comment;
+
+  public function __construct($data) {
+    $this->id = isset($data['id']) ? intval($data['id']) : null;
+    $this->comment = $data['comment'];
+  }
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Comment';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $theComment =  new Comment($row);
+      array_push($arr, $theComment);
+    }
+    return $arr;
+  }
+
+  public function create() {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    $sql = 'INSERT Comment (id, comment)
+            VALUES (?, ?)';
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->id,
+      $this->comment,
+    ]);
+    $this->id = $db->lastInsertId();
+  }
+}
