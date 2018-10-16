cbddb7bf176f5887659823d057e13c1c052df3f4
akmarche <42591895+akmarche@users.noreply.github.com>
2018-10-16 16:55:16 -0400

    Fixed comments table name

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -34,7 +34,7 @@ class Comment
     // 1. Connect to the database
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     // 2. Prepare the query
-    $sql = 'SELECT * FROM Comment';
+    $sql = 'SELECT * FROM comments';
     $statement = $db->prepare($sql);
     // 3. Run the query
     $success = $statement->execute(
